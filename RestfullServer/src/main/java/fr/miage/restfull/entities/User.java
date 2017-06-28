022dbc9aacdd92ea87391a4f95c7d711be9f8e0a
nicomahery <nicomahery@gmail.com>
2017-06-28 22:06:18 +0200

    Minor modifications (warning suppression)

--- RestfullServer/src/main/java/fr/miage/restfull/entities/User.java
+++ RestfullServer/src/main/java/fr/miage/restfull/entities/User.java
@@ -1,7 +1,6 @@
 package fr.miage.restfull.entities;
 
 import java.io.Serializable;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
